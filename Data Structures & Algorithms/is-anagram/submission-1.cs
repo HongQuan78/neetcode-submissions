5e5f01ef10043068d50e50b81f919dd07703a783
Vo Hong Quan <84892959+HongQuan78@users.noreply.github.com>
2026-04-15 23:35:26 +0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length)
+        {
+            return false;
+        }
+
+        int[] charCounts = new int[26];
+
+        foreach(char c in s)
+        {
+            charCounts[c - 'a']++;
+        }
+
+        foreach(char c in t)
+        {
+            int index = c - 'a';
+            
+            charCounts[index]--;
+
+            if(charCounts[index] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+        
+    }
+}
