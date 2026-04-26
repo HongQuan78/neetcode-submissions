fa3a1a500330ceb2d1d765d18f3a057b61b2fc28
Vo Hong Quan <84892959+HongQuan78@users.noreply.github.com>
2026-04-26 09:05:38 +0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+        while(left < right)
+        {   
+            int sum = numbers[left] + numbers[right];
+            if(sum < target)
+            {
+                left++;
+                continue;
+            }
+            else if(sum > target)
+            {
+                right--;
+                continue;
+            }
+            else
+            {
+                return new int[] {left + 1, right + 1};
+            }
+        }
+
+        return new int[] {0, 0};
+    }
+}
